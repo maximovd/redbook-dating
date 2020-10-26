247970bc54e14fbce0f0c7d6589adb20062dff4a
Danil Maximov <jokjokjokerface@gmail.com>
2020-10-26 22:57:55 +0300

    Create custom manager to User model

---
+++ redbook_dating/users/managers.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user manager where email is the unique id for auth instead
+    of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """Create and save user with the given email and password.
+
+        Args:
+            email: User email
+            password: User password
+
+        Returns:
+            New User
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save SuperUser with the given and password.
+
+        Args:
+            email: SuperUser email
+            password: SuperUser password
+
+        Returns:
+            New SuperUser
+        """
+        extra_fields.setdefault('is_stuff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_status=True'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True'))
+        return self.create_user(email, password, **extra_fields)
